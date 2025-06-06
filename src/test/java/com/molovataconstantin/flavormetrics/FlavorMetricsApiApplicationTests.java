8e1fcd4c40820b13fed4a66171d31d40b9d81bc2
Ghost420 <molovataconstantin@gmail.com>
2025-06-06 14:50:59 +0200

    first commit

---
+++ src/test/java/com/molovataconstantin/flavormetrics/FlavorMetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molovataconstantin.flavormetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorMetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
